5f3875b694b9b452b14370e41e7558370832ed58
anoopsharma160@gmail.com <anoopsharma160@gmail.com>
2018-12-03 20:14:22 +0530

    jar support

--- src/test/java/IncOiIncPriTest.java
+++ src/test/java/IncOiIncPriTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class IncOiIncPriTest {
-}
+//import io.restassured.RestAssured;
+//import io.restassured.response.Response;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class IncOiIncPriTest {
+//    @Test(invocationCount = 1000000)
+//    void callIncPriIncOi() throws InterruptedException {
+//        Thread.sleep(100000);
+//        Response response = RestAssured.given().port(9000).get("/dashboard/IncPriIncOi/0");
+//        Assert.assertEquals(response.getStatusCode(), 200);
+//
+//    }
+//}
